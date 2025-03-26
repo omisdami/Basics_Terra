82216548e614d7f5dad510a82336df9641fd2fd2
omisdami <omisdami@gmail.com>
2025-03-26 17:22:30 -0400

    changes

--- terraform variables exp/Variable.tf
+++ terraform variables exp/Variable.tf
@@ -7,12 +7,12 @@ default = "us-east-2"
 }
 
 variable "Security_Group" {
-    type = "list"
+    type = list
     default = ["sg-0c1f7b7b4c4b4b4b4"]
   
 }
 variable "AMIS" {
-    type = "map"
+    type = map
     default = {
       us-east-1 = "ami-084568db4383264d4"
       us-east-2 = "ami-04f167a56786e4b09"

8c860b68346882428df7733c6998f08c6cf7c3a4
omisdami <omisdami@gmail.com>
2025-03-26 02:29:20 -0400

    chnages

--- createInstance.tf
+++ createInstance.tf
@@ -1,8 +1,4 @@
-provider "aws" {
-  region     = "us-west-2"
-  access_key = "AKIA4VDBL4UNYWNVEWMN"
-  secret_key = "my-secret-key"
-}
+
 
 resource "aws_instance" "MyFirstInstnace" {
   ami           = "ami-0d0f28110d16ee7d6"

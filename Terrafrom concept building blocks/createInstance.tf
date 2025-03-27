7f57dca3163728e3a88521790b20c85282454170
omisdami <omisdami@gmail.com>
2025-03-26 23:53:55 -0400

    changes

--- Terrafrom concept building blocks/createInstance.tf
+++ Terrafrom concept building blocks/createInstance.tf
@@ -7,10 +7,6 @@ data "aws_ami" "amazon_linux" {
   most_recent = true
   owners      = ["amazon"]
 
-  filter {
-    name   = "name"
-    values = ["al2023-ami-2023*"]
-  }
 }
 resource "aws_key_pair" "levelup_key" {
     key_name = "levelup_key"

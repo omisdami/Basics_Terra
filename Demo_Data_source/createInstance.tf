22b794f84f5a7ad0d3c15c99055650d200ddfef2
omisdami <omisdami@gmail.com>
2025-03-27 02:47:31 -0400

    changes

--- Demo_Data_source/createInstance.tf
+++ Demo_Data_source/createInstance.tf
@@ -3,11 +3,16 @@ data "aws_availability_zones" "avilable" {}
 data "aws_ami" "amazon_linux" {
   most_recent = true
   owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*"]
+  }
 }
 
 resource "aws_instance" "MyFirstInstnace" {
   instance_type = "t2.micro"
   availability_zone = data.aws_availability_zones.avilable.names[1]
+  ami = data.aws_ami.amazon_linux.id
 
   tags = {
     Name = "custom_instance"
